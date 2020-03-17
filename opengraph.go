4b2fb08f56f673f019ce9932abfa1df2c74c62a0
izzatzr <izzatzr97@gmail.com>
2020-03-17 20:27:58 +0700

    Rewrite source structure separating the requests function into different file by category
    Signed-off-by: izzatzr <izzatzr97@gmail.com>

---
+++ opengraph.go
@@ -0,0 +1,13 @@
+package covid19
+
+/* OpenGraph download images
+// TODO
+func (api options) GetOpenGraph() *resty.Response {
+	url := api.url + "/og"
+	resp, err := api.req.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	return resp
+}
+*/
